48261a26512ca4d220934a73e1a3fa565a66681f
Barış Türe <baristure@Barss-MacBook-Air.local>
2020-06-16 22:41:37 +0300

    Header component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Book from './components/book/Book'
+import Header from './components/header/header'
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Book/>
     </div>
   );
